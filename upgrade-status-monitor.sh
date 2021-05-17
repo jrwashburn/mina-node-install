ad48eeeec1f72bb2c30084ebe7af33544cfd65f1
Jonathan Washburn <jrwashburn@gmail.com>
2021-05-16 20:51:51 -0400

    adding restart to upgarde status monitor

--- upgrade-status-monitor.sh
+++ upgrade-status-monitor.sh
@@ -7,3 +7,6 @@ sed -i "s/YOUR_SW_FEE/$YOUR_SW_FEE/g" scripts/mina-status-monitor.sh
 sed -i "s/YOUR_SW_ADDRESS/$YOUR_SW_ADDRESS/g" scripts/mina-status-monitor.sh
 
 sudo cp scripts/mina-status-monitor.sh /usr/local/bin/mina-status-monitor.sh
+systemctl --user stop mina-status-monitor.service
+sleep 5s
+systemctl --user start mina-status-monitor.service
