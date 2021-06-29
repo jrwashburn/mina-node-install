a130f3dcb8d8dd7a0efaef9736d18b62ff5526c5
Jonathan Washburn <jrwashburn@gmail.com>
2021-06-28 23:56:45 -0400

    upgrade mainnet to 1.2.0beta1

--- mina-upgrade.sh
+++ mina-upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
-MINA_VERSION=mina-mainnet=1.1.5-a42bdee
-ARCHIVE_VERSION=mina-archive=1.1.5-a42bdee
-SIDECAR_VERSION=mina-bp-stats-sidecar=1.1.5-a42bdee
+MINA_VERSION=mina-mainnet=1.2.0beta1-c856692
+ARCHIVE_VERSION=mina-archive-mainnet=1.2.0beta1-c856692
+SIDECAR_VERSION=mina-bp-stats-sidecar=1.2.0beta1-c856692
 
 systemctl --user stop mina-status-monitor.service
 systemctl --user stop mina-staking-ledgers-archive.timer
@@ -9,11 +9,11 @@ systemctl --user stop mina.service
 systemctl --user stop mina-archive.service
 systemctl --user stop mina-sidecar.service
 
-sudo apt-get remove -y mina-testnet-postake-medium-curves
-sudo apt-get remove -y mina-mainnet
-echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
+#echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
+#sudo apt-get -y update
+echo "deb [trusted=yes] http://packages.o1test.net stretch beta" | sudo tee /etc/apt/sources.list.d/mina-beta.list
 sudo apt-get -y update
-sudo apt-get install -y curl unzip $MINA_VERSION
+sudo apt-get install -y $MINA_VERSION
 sudo apt-get install -y $ARCHIVE_VERSION
 sudo apt-get install -y $SIDECAR_VERSION
 sudo cp partial-configs/mina-sidecar.json /etc/
