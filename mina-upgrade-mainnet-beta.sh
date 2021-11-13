ee41ae6734667891e5ceace6e96c2769678a42ce
Jonathan Washburn <jrwashburn@gmail.com>
2021-11-13 10:28:07 -0500

    update for 1.2.1 Beta

--- mina-upgrade-mainnet-beta.sh
+++ mina-upgrade-mainnet-beta.sh
@@ -1,20 +1,17 @@
 #!/bin/bash -x
-MINA_VERSION=mina-mainnet=1.2.0beta8-5b35b27
-ARCHIVE_VERSION=mina-archive-mainnet=1.2.0beta8-5b35b27
-SIDECAR_VERSION=mina-bp-stats-sidecar=1.2.0beta8-5b35b27
+MINA_VERSION=mina-mainnet=1.2.1beta1-45440dc
+ARCHIVE_VERSION=mina-archive-mainnet=1.2.1beta1-45440dc
 
 systemctl --user stop mina-status-monitor.service
 systemctl --user stop mina-staking-ledgers-archive.timer
 systemctl --user stop mina.service
 systemctl --user stop mina-archive.service
-systemctl --user stop mina-sidecar.service
 
 echo "deb [trusted=yes] http://packages.o1test.net stretch beta" | sudo tee /etc/apt/sources.list.d/mina-beta.list
 sudo apt-get -y update
 
 sudo apt-get install -y $MINA_VERSION
 sudo apt-get install -y $ARCHIVE_VERSION
-sudo apt-get install -y $SIDECAR_VERSION
 sudo cp partial-configs/mina-sidecar.json /etc/
 
 systemctl --user daemon-reload
@@ -23,5 +20,4 @@ systemctl --user start mina.service
 echo "going to sleeep for 5 minutes to let daemon bootstrap"
 sleep 5m
 systemctl --user start mina-staking-ledgers-archive.timer
-systemctl --user start mina-sidecar.service
 systemctl --user start mina-status-monitor.service
