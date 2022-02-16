fcbaa788739f5e422f6496a9bc59e1a45ffad14b
Jonathan Washburn <jrwashburn@gmail.com>
2022-02-16 12:36:06 -0500

    remove sidecar references

--- mina-upgrade-mainnet-beta.sh
+++ mina-upgrade-mainnet-beta.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 MINA_VERSION=mina-mainnet=1.3.0beta4-202caa6
 ARCHIVE_VERSION=mina-archive-mainnet=1.3.0beta4-202caa6
-SIDECAR_VERSION=mina-bp-stats-sidecar=1.3.0beta4-202caa6
 
 systemctl --user stop mina-status-monitor.service
 systemctl --user stop mina-staking-ledgers-archive.timer
@@ -13,7 +12,6 @@ sudo apt-get -y update
 
 sudo apt-get install -y $MINA_VERSION
 sudo apt-get install -y $ARCHIVE_VERSION
-sudo cp partial-configs/mina-sidecar.json /etc/
 
 systemctl --user daemon-reload
 systemctl --user start mina-archive.service
