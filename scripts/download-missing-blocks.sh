6c5054c82297a390ad727ccdf6c6afed97dc7edc
Jonathan Washburn <jrwashburn@gmail.com>
2024-01-21 19:16:54 -0600

    tweaked download missing blocks

--- scripts/download-missing-blocks.sh
+++ scripts/download-missing-blocks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # lifted from https://github.com/MinaProtocol/mina/blob/master/src/app/rosetta/download-missing-blocks.sh
 
-O1BLOCKS_BUCKET="${BLOCKS_BUCKET:=https://storage.googleapis.com/mina_network_block_data}"
-BLOCKS_BUCKET="${BLOCKS_BUCKET:=https://storage.googleapis.com/mina-mainnet-blocks}"
+O1BLOCKS_BUCKET=mina_network_block_data
+JKWH1_BLOCKS_BUCKET=mina-mainnet-blocks
 set -u
 
 MINA_NETWORK=${1}
@@ -31,20 +31,20 @@ function populate_db() {
 function download_block() {
   CHECKBLOCK=0
   echo "Downloading $1 block"
-  curl -sO "${BLOCKS_BUCKET}/${1}"
+  curl -sO "https://storage.googleapis.com/"${JKWH1_BLOCKS_BUCKET}/${1}
   CHECKBLOCK=$(grep '<Error><Code>NoSuchKey</Code>' $1 | wc -l)
   if [[ $CHECKBLOCK -eq 1 ]]; then
     CHECKBLOCK=0
     echo "Block $1 not found in bucket"
     echo "Downloading $1 block from O1 Labs"
-    curl -sO "${O1BLOCKS_BUCKET}/${1}"
+    curl -sO "https://storage.googleapis.com/"${O1BLOCKS_BUCKET}/${1}
     CHECKBLOCK=$(grep '<Error><Code>NoSuchKey</Code>' $1 | wc -l)
     if [[ $CHECKBLOCK -eq 1 ]]; then
       echo "Block $1 not found in either bucket"
       exit 1
     else    
       echo "Block $1 found in O1 Labs bucket - copying to Google Cloud Storage for retry"
-      gcloud storage cp $1 gs://storage.googleapis.com/mina-mainnet-blocks/
+      gcloud storage cp $1 gs://storage.googleapis.com/${JKWH1_BLOCKS_BUCKET}/
       rm $1
     fi
   else
@@ -55,25 +55,33 @@ function download_block() {
 HASH='map(select(.metadata.parent_hash != null and .metadata.parent_height != null)) | .[0].metadata.parent_hash'
 # Bootstrap finds every missing state hash in the database and imports them from the o1labs bucket of .json blocks
 function bootstrap() {
-  echo "[BOOTSTRAP] Restoring blocks individually from ${BLOCKS_BUCKET}..."
+  echo "[BOOTSTRAP] Restoring blocks individually from ${JKWH1_BLOCKS_BUCKET}..."
 
   until [[ "$PARENT" == "null" ]] ; do
     PARENT_FILE="${MINA_NETWORK}-$(mina-missing-blocks-auditor --archive-uri $PG_CONN | jq_parent_json)"
     if [[ "$PARENT_FILE" == "${MINA_NETWORK}-1-3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ.json" ]]; then
       echo "[BOOTSTRAP] Missing genesis block - continuing"
       PARENT_FILE="${MINA_NETWORK}-$(mina-missing-blocks-auditor --archive-uri $PG_CONN | jq_skip_parent_json)"
+      echo "[BOOTSTRAP] next block is " $PARENT_FILE
+      if [[ "$PARENT_FILE" == "mainnet-null-null.json" ]]; then
+        echo "[BOOTSTRAP] No more blocks to bootstrap"
+        PARENT=null
+      else
+        download_block "${PARENT_FILE}"
+        populate_db "$PG_CONN" "$PARENT_FILE"
+        PARENT="$(mina-missing-blocks-auditor --archive-uri $PG_CONN | jq_parent_json)"
+      fi
     fi
-    download_block "${PARENT_FILE}"
-    populate_db "$PG_CONN" "$PARENT_FILE"
-    PARENT="$(mina-missing-blocks-auditor --archive-uri $PG_CONN | jq_skip_parent_json)"
   done
 
   echo "[BOOTSTRAP] Top 10 blocks in bootstrapped archiveDB:"
   psql "${PG_CONN}" -c "SELECT state_hash,height FROM blocks ORDER BY height DESC LIMIT 10"
   echo "[BOOTSTRAP] This rosetta node is synced with no missing blocks back to genesis!"
 
-  echo "[BOOTSTRAP] Checking again in 60 minutes..."
-  sleep 3000
+  echo "[BOOTSTRAP] Checking again in 5 minutes..."
+  date
+  sleep 300
+  date
 }
 
 # Wait until there is a block missing
@@ -88,8 +96,6 @@ while true; do # Test once every 10 minutes forever, take an hour off when boots
 #  PARENT=$(echo "$output" | jq_parent_hash)
   PARENT="$(mina-missing-blocks-auditor --archive-uri $PG_CONN | jq_parent_hash)"
   echo ran mina-missing-blocks-auditor $PARENT
-  echo "[BOOTSTRAP] $(mina-missing-blocks-auditor --archive-uri $PG_CONN | jq -rs .[].message)"
   [[ "$PARENT" != "null" ]] && echo "[BOOSTRAP] Some blocks are missing, moving to recovery logic..." && bootstrap
-  sleep 6 # Wait for the daemon to catchup and start downloading new blocks
 done
 echo "[BOOTSTRAP] This rosetta node is synced with no missing blocks back to genesis!"
