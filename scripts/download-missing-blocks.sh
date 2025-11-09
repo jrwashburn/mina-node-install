b15b9e665c16dfa0cc912fc3f6be626bf073cd97
Jonathan Washburn <jrwashburn@gmail.com>
2025-11-09 16:53:22 -0500

    update missing blocks service to use an environment file

--- scripts/download-missing-blocks.sh
+++ scripts/download-missing-blocks.sh
@@ -2,12 +2,12 @@
 # lifted from https://github.com/MinaProtocol/mina/blob/master/src/app/rosetta/download-missing-blocks.sh
 
 O1BLOCKS_BUCKET=mina_network_block_data
-JKWH1_BLOCKS_BUCKET=mina-mainnet-blocks
+JKWH1_BLOCKS_BUCKET=$3
 set -u
 
-MINA_NETWORK=${1}
+MINA_NETWORK=$2
 # Postgres database connection string and related variables
-PG_CONN=${2}
+PG_CONN=$1
 
 function jq_parent_json() {
   jq -rs 'map(select(.metadata.parent_hash != null and .metadata.parent_height != null)) | "\(.[0].metadata.parent_height)-\(.[0].metadata.parent_hash).json"'
@@ -34,14 +34,15 @@ function download_block() {
   if [[ $CHECKBLOCK -eq 1 ]]; then
     CHECKBLOCK=0
     echo "Block $1 not found in bucket, will check O1 Labs bucket"
-    curl -sO "https://storage.googleapis.com/"${O1BLOCKS_BUCKET}/${1}
+    #curl -sO "https://storage.googleapis.com/"${O1BLOCKS_BUCKET}/${1}
+    curl -sO "https://storage.googleapis.com/"${O1BLOCKS_BUCKET}/mainnet-${1}
     CHECKBLOCK=$(grep '<Error><Code>NoSuchKey</Code>' $1 | wc -l)
     if [[ $CHECKBLOCK -eq 1 ]]; then
       echo "Block $1 not found in either bucket"
       exit 1
     else    
       echo "Block $1 found in O1 Labs bucket - copying to Google Cloud Storage for retry"
-      gcloud storage cp $1 gs://${JKWH1_BLOCKS_BUCKET}/
+      gcloud storage cp $1 gs://storage.googleapis.com/${JKWH1_BLOCKS_BUCKET}/
       rm $1
     fi
   else
@@ -72,9 +73,10 @@ function bootstrap() {
 PARENT=null
 while true; do # Test once every 5 minutes forever
   PARENT="$(mina-missing-blocks-auditor --archive-uri $PG_CONN | jq_parent_hash)"
+  echo Result from missing blocks auditor: $PARENT
   [[ "$PARENT" == "null" ]] && echo ran mina-missing-blocks-auditor and found no missing blocks.
   [[ "$PARENT" != "null" ]] && echo "[BOOSTRAP] Some blocks are missing, moving to recovery logic... starting with $PARENT" && bootstrap
-  echo "Sleeping for 5 minutes - will check again."
-  sleep 300
+  echo "Sleeping for 2 minutes - will check again."
+  sleep 120
 done
 echo "[BOOTSTRAP] This rosetta node is synced with no missing blocks back to genesis!"
