f9298fd6705ad1df58b0161710f7e04099cb8eaa
Jonathan Washburn <jrwashburn@gmail.com>
2025-12-28 15:45:15 -0500

    allow downgrades

--- mina-upgrade-mainnet-stable.sh
+++ mina-upgrade-mainnet-stable.sh
@@ -7,8 +7,8 @@ sudo rm /etc/apt/sources.list.d/mina*.list
 echo "deb [trusted=yes] http://packages.o1test.net $CODENAME stable" | sudo tee /etc/apt/sources.list.d/mina.list
 sudo apt-get -y update
 
-sudo apt-get install -y $MINA_VERSION
-sudo apt-get install -y $ARCHIVE_VERSION
+sudo apt-get install -y --allow-downgrades $MINA_VERSION
+sudo apt-get install -y --allow-downgrades $ARCHIVE_VERSION
 
 systemctl --user restart mina-archive.service
 systemctl --user restart mina.service
